660ae763994011d299c981ece0597d46ae4912f9
Jiaqi Liu <jiaqi.jql@gmail.com>
2016-10-15 05:28:47 -0400

    added api

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+class Article(Resource):
+    def post(self):
+        new_article = request.get_json(force=False)
+        is_relevant = False
+        data= {'relevancy': is_relevant, 'article': new_article}
+        return jsonify(data)
+
+api.add_resource(Article, '/article')
+
+if __name__ == "__main__":
+    app.run(debug=True)
