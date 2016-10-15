32a662c16d04955538be481e8eefb1c390455a1c
Jiaqi Liu <jiaqi.jql@gmail.com>
2016-10-15 08:39:50 -0400

    add app

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import os
+import urllib2
+
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+
+from goose import Goose
+
+g = Goose()
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,6 +20,17 @@ class Article(Resource):
         new_article = request.get_json(force=False)
         is_relevant = False
         data= {'relevancy': is_relevant, 'article': new_article}
+        return jsonify(data)
+
+    def get(self):
+        url = request.args.get('url')
+        print(url)
+        article = g.extract(url=url)
+        title = article.title
+        text = article.cleaned_text
+        is_relevant = False
+
+        data = {'title': title, 'text': text, 'relevancy': is_relevant}
         return jsonify(data)
 
 api.add_resource(Article, '/article')
